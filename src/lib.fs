3955a54f9f0c8493e9138e43be71436d64ddb1f8
ncave <777696+ncave@users.noreply.github.com>
2022-07-27 12:44:51 -0700

    Added lib.fs

---
+++ src/lib.fs
@@ -0,0 +1,32 @@
+module RayTracer
+
+#if FABLE_COMPILER_RUST
+
+open Fable.Core
+open Fable.Core.Rust
+open Imports
+
+import "wasm_bindgen::prelude::*" ""
+
+let maxWidth = 2048
+let maxHeight = 2048
+let bufferLength = maxWidth * maxHeight * 4
+let buffer = Array.zeroCreate<byte> bufferLength
+
+[<Emit("*const u8")>]
+type BufferType = BufferType of byte[]
+
+[<Emit("$0.as_ptr()")>]
+let ptrOf x: BufferType = nativeOnly
+
+[<OuterAttr("wasm_bindgen")>]
+let get_buffer_length () = bufferLength
+
+[<OuterAttr("wasm_bindgen")>]
+let get_buffer_offset () = ptrOf buffer
+
+[<OuterAttr("wasm_bindgen")>]
+let render_scene(x, y, w, h, angle) =
+    RayTracerDemo.renderScene(buffer, x, y, w, h, angle)
+
+#endif //FABLE_COMPILER_RUST
