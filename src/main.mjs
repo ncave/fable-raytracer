24b5140ca2690e225b1ae51961301df81174ba7a
ncave <777696+ncave@users.noreply.github.com>
2023-10-14 09:44:15 -0700

    Updated to .NET 8.0

--- src/main.mjs
+++ src/main.mjs
@@ -1,2 +1,23 @@
-import { dotnet } from './dotnet.js'
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+import { dotnet } from './_framework/dotnet.js'
+
+// const { setModuleImports, getAssemblyExports, getConfig } = await dotnet
+//     .withDiagnosticTracing(false)
+//     .create();
+
+// setModuleImports('main.mjs', {
+//     node: {
+//         process: {
+//             version: () => globalThis.process.version
+//         }
+//     }
+// });
+
+// const config = getConfig();
+// const exports = await getAssemblyExports(config.mainAssemblyName);
+// const text = exports.MyClass.Greeting();
+// console.log(text);
+
 await dotnet.run();
